2ab215f840772d543fbeb64012a202ede2a6f041
Hamed Nourhani <info@itstar.ir>
2015-07-11 15:01:41 +0430

    first commit

---
+++ config.rb
@@ -0,0 +1,7 @@
+require 'susy'
+css_dir = "_/css"          #where the CSS will saved
+sass_dir = "_/components/sass"           #where our .scss files are
+javascripts = "_/js"
+images_dir = "_/images"
+output_style = :nested
+relative_assets = true
