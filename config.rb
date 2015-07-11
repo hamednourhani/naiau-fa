4e9ddc1f9b1a7dc1e00ce3b0523e6834dfb28496
Hamed Nourhani <info@itstar.ir>
2015-07-11 15:44:09 +0430

    somechange

--- config.rb
+++ config.rb
@@ -1,6 +1,7 @@
 require 'susy'
 css_dir = "_/css"          #where the CSS will saved
-sass_dir = "_/components/sass"           #where our .scss files are
+sass_dir = "_/components/sass" 
+fonts_dir = "_/fonts"          #where our .scss files are
 javascripts = "_/js"
 images_dir = "_/images"
 output_style = :nested
